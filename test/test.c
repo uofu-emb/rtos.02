09a548727d478921f111256f241ddc46953b9ad1
Ashton Snelgrove <ashton.snelgrove@utah.edu>
2024-09-08 11:46:51 -0600

    Attempting to get the renode simulator to boot.

--- test/test.c
+++ test/test.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main(void)
-{
-    while(1);
-    /* stdio_init_all(); */
-
-    /* while (true) { */
-    /*     printf("Hello, world!\n"); */
-    /*     sleep_ms(1000); */
-    /* } */
+int main() {
+    stdio_init_all();
+    while (1) {
+        printf("hello world!\n");
+        sleep_ms(1000);
+    }
     return 0;
 }
