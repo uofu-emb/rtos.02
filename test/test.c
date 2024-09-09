b5043bd24a5e4f6b8f94e38315fd8500bab4e418
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-09 11:01:31 -0600

    Add unity demo

--- test/test.c
+++ test/test.c
@@ -1,11 +1,28 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include <stdint.h>
+#include <unity.h>
 
-int main() {
-    stdio_init_all();
-    while (1) {
-        printf("hello world!\n");
-        sleep_ms(1000);
-    }
-    return 0;
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_variable_assignment()
+{
+    int x = 1;
+    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+}
+
+void test_multiplication(void)
+{
+    int x = 5;
+    int y = 6;
+    int z = x * y;
+    TEST_ASSERT_TRUE_MESSAGE(z == 30, "Multiplication of two integers returned incorrect value.");
+}
+
+int main (void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_variable_assignment);
+    RUN_TEST(test_multiplication);
+    return UNITY_END();
 }
