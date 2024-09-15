3563ca3dbbab35b9bfb7b848fc94ff917fbc2cb3
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-14 20:28:18 -0600

    Strip comments and embed function.

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -13,15 +13,12 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-// Delay between led blinking
 #define LED_DELAY_MS 2000
 
-// Priorities of our threads - higher numbers are higher priority
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
 #define WORKER_TASK_PRIORITY    ( tskIDLE_PRIORITY + 4UL )
 
-// Stack sizes of our threads in words (4 bytes)
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define WORKER_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
@@ -46,15 +43,6 @@ void main_task(__unused void *params) {
     }
 }
 
-void vLaunch( void) {
-    TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
-
-
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
-}
-
 int main( void )
 {
     stdio_init_all();
@@ -62,6 +50,8 @@ int main( void )
     /* Configure the hardware ready to run the demo. */
     const char *rtos_name;
     rtos_name = "FreeRTOS";
-    vLaunch();
+    TaskHandle_t task;
+    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    vTaskStartScheduler();
     return 0;
 }
