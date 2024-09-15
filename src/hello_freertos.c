3110de3b7bd4528e395d92d0d1aeeb93cbd1487b
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-14 20:53:02 -0600

    Final blocks

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -13,45 +13,42 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-#define LED_DELAY_MS 2000
+int count = 0;
+bool on = false;
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
-#define WORKER_TASK_PRIORITY    ( tskIDLE_PRIORITY + 4UL )
-
+#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define WORKER_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
 void blink_task(__unused void *params) {
-    bool on = false;
-    printf("blink_task starts\n");
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        on = !on;
-        sleep_ms(LED_DELAY_MS);
+        if (count++ % 11) on = !on;
+        vTaskDelay(500);
     }
 }
 
 void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    int count = 0;
-    while(true) {
-        printf("Hello from main task count=%u\n", count++);
-        vTaskDelay(3000);
+    xTaskCreate(blink_task, "BlinkThread",
+                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    char c;
+    while(c = getchar()) {
+        if (c <= 'z' && c >= 'a') putchar(c - 32);
+        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
+        else putchar(c);
     }
 }
 
 int main( void )
 {
     stdio_init_all();
-
-    /* Configure the hardware ready to run the demo. */
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    xTaskCreate(main_task, "MainThread",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
     vTaskStartScheduler();
     return 0;
 }
